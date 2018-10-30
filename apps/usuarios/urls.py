8d20ef4b5dd91e850a14153e85a3f8c52658ca3e
myexoplanet <dantemichell@gmail.com>
2018-10-30 10:24:13 -0300

    Fixed bug urls usuarios

--- apps/usuarios/urls.py
+++ apps/usuarios/urls.py
@@ -4,14 +4,11 @@ from . import views
 
 urlpatterns = [   
 	
-	url(r'^index/', views.index, name='index'),  
-	
-	url(r'^administrar/', views.administrar, name='administrar'),  	  	
+	url(r'^administrar/$', views.administrar, name='administrar'),  	  	
 	url(r'^$', views.index,name='index'),
-	url('',views.index,name='index'),
-	url(r'^administracion/', views.administracion, name='administracion'),	
-	url(r'^login/', views.login, name='login'),	
-	url(r'^registro/', views.registro, name='registro'),
-	url(r'^recuperacion/', views.recuperacion, name='recuperacion'),
-	url(r'^zonas/', views.zonas, name='zonas'), 
+	url(r'^administracion/$', views.administracion, name='administracion'),	
+	url(r'^login/$', views.login, name='login'),	
+	url(r'^registro/$', views.registro, name='registro'),
+	url(r'^recuperacion/$', views.recuperacion, name='recuperacion'),
+	url(r'^zonas/$', views.zonas, name='zonas'), 
 	]
